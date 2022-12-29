14ff10e423b598a6735d07245de2a26de0d0d0e3
Blessing Olusanya <debbyblessing1234@gmail.com>
2022-12-29 11:23:27 +0100

    Adding all Select through JS feature

---
+++ index.js
@@ -0,0 +1,17 @@
+const selectMenu = document.querySelectorAll('select');
+
+for (let i = 12; i > 0; i--) {
+  i = i < 10 ? `0${i}` : i;
+  const option = `<option value="${i}">${i}</option>`;
+  selectMenu[0].firstElementChild.insertAdjacentHTML('afterend', option);
+}
+for (let i = 59; i > 0; i--) {
+  i = i < 10 ? `0${i}` : i;
+  const option = `<option value="${i}">${i}</option>`;
+  selectMenu[1].firstElementChild.insertAdjacentHTML('afterend', option);
+}
+for (let i = 2; i > 0; i--) {
+  const ampm = i === 1 ? 'AM' : 'PM';
+  const option = `<option value="${ampm}">${ampm}</option>`;
+  selectMenu[2].firstElementChild.insertAdjacentHTML('afterend', option);
+}
