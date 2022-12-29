060fef656cb60d7bd18e157f520c15154981f68e
Blessing Olusanya <debbyblessing1234@gmail.com>
2022-12-29 12:04:48 +0100

    adding current time in digital format

--- index.js
+++ index.js
@@ -15,3 +15,30 @@ for (let i = 2; i > 0; i--) {
   const option = `<option value="${ampm}">${ampm}</option>`;
   selectMenu[2].firstElementChild.insertAdjacentHTML('afterend', option);
 }
+
+setInterval(() => {
+  // getting hour, mins, secs
+  const date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let ampm = 'AM';
+
+  if (h >= 12) {
+    h = h - 12;
+    ampm = 'PM';
+  }
+  // if hour value is 0, set this value to 12
+  if (h === 0) {
+    h = 12;
+  }
+  // adding 0 before hr, min, sec if thi value is less than 10
+  if (h < 10) {
+    h += '0';
+  } else if (m < 10) {
+    m += '0';
+  } else if (s < 10) {
+    s += '0';
+  }
+  console.log(`${h}:${m}:${s} ${ampm}`);
+}, 1000);
