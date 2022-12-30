e0efc64d43a2b04831459c15d595e76a8bb9fb91
Blessing Olusanya <debbyblessing1234@gmail.com>
2022-12-30 11:28:40 +0100

    add falase to isAlarmSet feature

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const content = document.querySelector('.content');
 const setAlarmBtn = document.querySelector('button');
 
 let alarmTime;
+let isAlarmSet = false;
 let time;
+const ringtone = new Audio('file/alarm clock.mp3');
 for (let i = 12; i > 0; i--) {
   i = i < 10 ? `0${i}` : i;
   const option = `<option value="${i}">${i}</option>`;
@@ -21,16 +23,6 @@ for (let i = 2; i > 0; i--) {
   selectMenu[2].firstElementChild.insertAdjacentHTML('afterend', option);
 }
 
-const setAlarm = () => {
-  // getting hour, minute, ampm select tag value
-  time = ` ${selectMenu[0].value}: ${selectMenu[1].value} : ${selectMenu[2].value}`;
-  if (time.includes('Hour') || time.includes('Minute') || time.includes('AM/PM')) {
-    alert('Please,select a valid time to set alarm');
-  }
-
-  content.classList.add('disable');
-  setAlarmBtn.innerHTML = 'Clear Alarm';
-};
 
 setInterval(() => {
   // getting hour, mins, secs
@@ -56,8 +48,29 @@ setInterval(() => {
   // getting exact time
 
   if (alarmTime === ` ${h}: ${m} : ${ampm}`) {
+    ringtone.play();
+    ringtone.loop = true;
     console.log('alarm ringing');
   }
 }, 1000);
+const setAlarm = () => {
+  if (isAlarmSet) { // isAlarmSet is true
+    alarmTime = ''; //
+    ringtone.pause();
+    content.classList.remove('disable');
+    setAlarmBtn.innerHTML = 'Set Alarm';
+    // return isAlarmSet value to false
+    const set = isAlarmSet = false;
+    return set;
+  }
+  // getting hour, minute, ampm select tag value
+  time = ` ${selectMenu[0].value}: ${selectMenu[1].value} : ${selectMenu[2].value}`;
+  if (time.includes('Hour') || time.includes('Minute') || time.includes('AM/PM')) {
+    alert('Please,select a valid time to set alarm');
+  }
+  isAlarmSet = true;
+  content.classList.add('disable');
+  setAlarmBtn.innerHTML = 'Clear Alarm';
+};
 
 setAlarmBtn.addEventListener('click', setAlarm);
